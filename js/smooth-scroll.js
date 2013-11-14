bbec1366f38045b59583c9d0cb4d1c32112bcb1e
Guillermo Ceballos <willeeh@gmail.com>
2013-11-14 20:41:35 +0100

    Initial commit

---
+++ js/smooth-scroll.js
@@ -0,0 +1,8 @@
+(function($) {
+    jQuery(document).ready(function($) {
+        $(".scroll").click(function(event){ // When a link with the .scroll class is clicked
+            event.preventDefault(); // Prevent the default action from occurring
+            $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500); // Animate the scroll to this link's href value
+        });
+    });
+})(jQuery);
