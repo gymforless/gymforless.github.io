f5df7c731209b729a7d93f90e3a6c3df90be298a
Guillermo Ceballos <willeeh@gmail.com>
2013-11-17 17:22:07 +0100

    fixed a minor issue

--- js/g4l.js
+++ js/g4l.js
@@ -8,9 +8,9 @@
 
 	$(function(){
 		if (isMobileWebkit) {
-            $('.context-1').setAttribute('style', 'background-position: 60% -200px;');
-            $('.context-2').setAttribute('style', 'background-position: 48% -320px;');
-            $('.context-3').setAttribute('style', 'background-position: 48% -300px;');
+            $('.context-1').css('background-position', '60% -200px');
+            $('.context-2').css('background-position', '48% -320px');
+            $('.context-3').css('background-position', '48% -300px');
 		} else {
 			$.stellar({
                 horizontalScrolling: false,
